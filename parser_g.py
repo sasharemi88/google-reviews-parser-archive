25d07632ba396048631b4c207212c1728722378f
sasharemi88 <63388765+sasharemi88@users.noreply.github.com>
2020-04-28 15:24:48 +0500

    28-04

--- parser_g.py
+++ parser_g.py
@@ -8,28 +8,13 @@ import time
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
-import os
-#from utils import get_cvmde_path, write_to_db
 
-# Установить текущую папку как рабочую директорию
-#work_dir = os.path.dirname(os.path.realpath(__file__))
-#os.chdir(work_dir)
-
-# Названия файлов для входных/выходных данных
-f_input = 'sverdl_objects.csv'
-f_output = 'google-reviews-parser_avg_' + time.strftime('%Y%m%d%H%M%S') + '.csv'
-
-# Установить конечный путь для результатов
-#cvmde_path = get_cvmde_path()
-#output_dir = os.path.join(cvmde_path, 'data/scrapy/google-reviews-parser')
-output_dir = ''
-#os.makedirs(output_dir, exist_ok=True)
-
-file_output = os.path.join(output_dir, f_output)
+file_input = 'sverdl_objects.csv'
+file_output = 'google_smarks_avg_' + time.strftime('%Y%m%d%H%M%S') + '.csv'
 
 #приведение кластеризованной семантики к двумерному массиву
 semant_clast = []
-with open(f_input, 'r', newline='', encoding='1251') as File_semant:
+with open(file_input, 'r', newline='') as File_semant:
     reader = csv.reader(File_semant, delimiter=';')
     for row in reader:
         semant_clast.append(row)
@@ -41,7 +26,7 @@ for i in range(1, len(semant_clast)):
 
 #Создаем словарь: ключ - объект, значение - ['регион','город','категория']
 semant_dict = dict()
-with open(f_input, 'r', newline='', encoding='1251') as File_region:
+with open(file_input, 'r', newline='') as File_region:
     reader = csv.DictReader(File_region, delimiter=';')
     for line in reader:
         semant_dict[line["object"]] = [line["region"], line["city"], line["cat"], line["comerc"]]
@@ -50,7 +35,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) 
 
 #создаем файл с оценками
 with open(file_output, 'w', newline='') as file:
-    fields = ['region', 'city', 'cat', 'comerc', 'object', 'mark']
+    fields = ['region', 'city', 'cat', 'comerc', 'object', 'mark', 'cnt_marks']
     writer = csv.DictWriter(file, fieldnames=fields, delimiter=';')
     writer.writeheader()
     for line in objects:
@@ -58,16 +43,21 @@ with open(file_output, 'w', newline='') as file:
         r = requests.get('https://www.google.com/search?q=' + line + ' ' + semant_dict[line][1] + '&oq=' + line + ' ' + semant_dict[line][1] + '&aqs=chrome.0.69i59j69i65j0l4.4212j0j7&sourceid=chrome&ie=UTF-8', headers = headers)
         time.sleep(1)
         soup = bs(r.text, 'lxml')
-        mark = str(soup.select("span.Aq14fc"))
-        s = mark.find('>')
-        f = mark.find('<',s)
+        mark = str(soup.select("span.Aq14fc")) #средняя оценка
+        sm = mark.find('>')
+        fm = mark.find('<',sm)
+        cnt = str(soup.select("span.hqzQac span a span")) #кол-во оценок
+        sc = cnt.find('>')
+        fc = cnt.find(' ', sc)
         writer.writerow({'region':semant_dict[line][0],
                          'city':semant_dict[line][1],
                          'cat':semant_dict[line][2],
                          'comerc':semant_dict[line][3],
                          'object':line,
-                         'mark': mark[s+1:f]})
-        print(line + semant_dict[line][1] + ' ' + mark[s+1:f])
+                         'mark': mark[sm+1:fm],
+                         'cnt_marks': cnt[sc+1:fc]})
+        print(line + semant_dict[line][1] + ' ' + mark[sm+1:fm])
+        print(cnt[sc+1:fc])
         print('____')
         time.sleep(1)
         
